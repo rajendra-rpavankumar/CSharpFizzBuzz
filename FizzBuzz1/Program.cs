b3d833fbea800479a7252a636172683f6116cf9d
rajendra-rpavankumar <rajendra.rpavankumar@gmail.com>
2017-08-15 11:25:20 +0100

    Adding files

---
+++ FizzBuzz1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0) result = "Fizz";
+                if (i % 5 == 0) result = result +"Buzz";
+                if (result.Length == 0) result += i;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("FizzBuzz Soltion");
+            for(int i=1; i<=75; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                //else if (i % 3 == 0 && i % 5 == 0)
+                //    Console.WriteLine("FizzBuzz");
+                else
+                    Console.WriteLine(i);
+               
+            }
+            Console.Read();
+        }
+    }
+}
